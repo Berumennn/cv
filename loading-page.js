64a71a2d2cffe7fac07f2b6f6c3c55a14de339dc
Berumennn <robertoberumen463@gmail.com>
2024-05-27 12:04:42 -0600

    Create loading-page.js

---
+++ loading-page.js
@@ -0,0 +1,13 @@
+// script.js
+document.addEventListener('DOMContentLoaded', function() {
+    // Simulate a loading delay (optional)
+    setTimeout(function() {
+        document.getElementById('loader').style.display = 'none';
+        document.getElementById('start-button').style.display = 'block';
+    }, 3000); // Adjust the delay as needed
+});
+
+document.getElementById('start-button').addEventListener('click', function() {
+    document.getElementById('loader-wrapper').style.display = 'none';
+    document.getElementById('content').style.display = 'block';
+});
